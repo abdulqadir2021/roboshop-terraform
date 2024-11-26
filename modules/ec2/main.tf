f7e0a2a9548a1655a8b37124e2254f7b30a3131c
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2024-11-27 05:27:16 +0530

    Install config changed to shell

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -54,7 +54,7 @@ resource "aws_route53_record" "record" {
   name    = "{var.component_name}-${var.env}.${var.domain_name}"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.mongodb.private_ip]
+  records = [aws_instance.instance.private_ip]
 }
 
 
