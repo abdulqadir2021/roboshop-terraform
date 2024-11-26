8891ebb728f76d119ec2908b48ed5a2d9409169a
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2024-11-27 03:55:55 +0530

    Install config changed to shell

--- main.tf
+++ main.tf
@@ -18,11 +18,11 @@ module "app_instances" {
 }
 
 module "web_instances" {
-  depends_on      = [module.app_instances]
-  for_each        = var.web_instances
-  source          = "./modules/ec2"
-  env             = var.env
-  app_port        = each.value["app_port"]
-  component_name  = each.key
-  instance_type   = each.value["instance_type"]
+  depends_on     = [module.app_instances]
+  for_each       = var.web_instances
+  source         = "./modules/ec2"
+  env            = var.env
+  app_port       = each.value["app_port"]
+  component_name = each.key
+  instance_type  = each.value["instance_type"]
 }
