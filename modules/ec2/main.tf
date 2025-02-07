8b2d68bfb4958f54cefab1dc28e9880c55f19ca4
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2025-02-07 20:01:10 +0530

    project roboshop

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -43,7 +43,6 @@ resource "null_resource" "ansible-pull" {
     }
 
     inline = [
-      "sudo labauto ansible",
       "ansible-pull -i localhost, -U https://github.com/abdulqadir2021/roboshop-ansible roboshop.yml -e env=${var.env} -e component=${var.component_name} -e vault_token=${var.vault_token}"
     ]
   }
