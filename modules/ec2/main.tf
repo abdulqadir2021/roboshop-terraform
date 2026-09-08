fe2feb64e381ba9fa8c49a5b134d6fbb1ea9e07b
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2026-09-08 18:43:21 +0530

    project roboshop 26

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -32,9 +32,9 @@ resource "aws_instance" "instance" {
   tags = {
     Name = "${var.component_name}-${var.env}"
   }
-  root_block_device {
-    volume_size = var.volume_size
-  }
+  # root_block_device {
+  #   volume_size = var.volume_size
+  # }
 
   lifecycle {
     ignore_changes = [
