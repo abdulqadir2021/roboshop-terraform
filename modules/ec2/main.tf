876b7b18932ea6ddb32354310d0ea9754c6de1ad
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2026-09-15 17:44:46 +0530

    project roboshop 26

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -60,6 +60,7 @@ resource "null_resource" "ansible-pull" {
     }
 
     inline = [
+      "sudo dnf install ansible-core -y",
       "ansible-pull -i localhost, -U https://github.com/abdulqadir2021/roboshop-ansible roboshop.yml -e env=${var.env} -e component=${var.component_name} -e vault_token=${var.vault_token}"
     ]
   }
