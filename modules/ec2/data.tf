aa84fb08e463ef16261ec02ec86430546cb27e4c
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2026-09-23 18:17:11 +0530

    project roboshop 26

--- modules/ec2/data.tf
+++ modules/ec2/data.tf
@@ -1,14 +1,14 @@
-data "aws_ami" "ami" {
-  most_recent = true
-  name_regex  = "Redhat-9-DevOps-Practice"
-  owners      = ["973714476881"]
-}
-
 # data "aws_ami" "ami" {
 #   most_recent = true
-#   name_regex  = "rhel-golden-image"
-#   owners      = ["self"]
+#   name_regex  = "Redhat-9-DevOps-Practice"
+#   owners      = ["973714476881"]
 # }
+
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "rhel-golden-image"
+  owners      = ["self"]
+}
 
 
 data "vault_generic_secret" "ssh" {
