0ab09e740fcaddee27061644603b62fa50506123
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2026-09-25 16:48:44 +0530

    project roboshop 26

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -60,6 +60,7 @@ resource "null_resource" "ansible-pull" {
     }
 
     inline = [
+      "sudo pip3.13 install hvac --break-system-packages 2>/dev/null || sudo pip install hvac",
       "ansible-pull -i localhost, -U https://github.com/abdulqadir2021/roboshop-ansible roboshop.yml -e env=${var.env} -e component=${var.component_name} -e vault_token=${var.vault_token}"
     ]
   }
